8f8bbc6605e26d55a6f7ddf17970a5ef4368ef60
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-14 19:26:23 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+	public static  class Product{
+		
+		public int Product(int x,int y) {
+			
+			return x*y;
+			
+		}
+		public int Product(int x,int y,int z) {
+			
+			return x*y*z;
+		}
+		
+		public double product(double x, double y) {
+			
+			 return x * y;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Product p= new Product();
+		System.out.println(p.Product(5, 6));
+		System.out.println(p.Product(6 , 7, 8));
+		System.out.println(p.product(9, 9));
+		
+	}
+
 }
