cb6d8ae7e444c254810e809d485dc7ceacef699c
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-14 19:35:37 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,17 +6,17 @@ public class Main {
 		
 		public int Product(int x,int y) {
 			
-			return x*y;
+			
 			
 		}
 		public int Product(int x,int y,int z) {
 			
-			return x*y*z;
+			
 		}
 		
 		public double product(double x, double y) {
 			
-			 return x * y;
+			
 		}
 	}
 	public static void main(String[] args) {
