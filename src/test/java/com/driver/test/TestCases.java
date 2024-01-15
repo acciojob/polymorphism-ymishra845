5d803d941768ec9c5cdd056ede6e33aac25d081a
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-15 09:46:45 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,20 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
-public class TestCases {
-     
-}
+public static  class Product{
+		
+		public int Product(int x,int y) {
+			
+			return x*y;
+			
+		}
+		public int Product(int x,int y,int z) {
+			
+			return x*y*z;
+		}
+		
+		public double product(double x, double y) {
+			return x*y;
+			
+		}
+	}
