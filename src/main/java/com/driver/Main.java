739e41f58998f70e7013df646451da5f47e9dd8a
ymishra845 <ymishra845@gmail.com>
2024-01-15 21:04:43 +0530

    polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,23 @@ package com.driver;
 
 public class Main {
 
+public static  class Product{
+		
+		public int Product(int x,int y) {
+			
+			return x*y;
+			
+		}
+		public int Product(int x,int y,int z) {
+			
+			return x*y*z;
+		}
+		
+		public double product(double x, double y) {
+			return x*y;
+			
+		}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
